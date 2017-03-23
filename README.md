e44e8d50ecc4035946384c3a712905817d1ec0f5
tchiotludo <tchiot.ludo@gmail.com>
2017-03-23 14:18:37 +0100

    Add network variable config
    - ES_NETWORK_PUBLISH_HOST_DETECT_REGEXP
    - ES_NETWORK_PUBLISH_HOST
    - ES_ZEN_MINIMUM_MASTER
    - ES_ZEN_PING_UNICAST_HOSTS

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Docker Elasticsearch
 
 ## Environment Variables
+
+* `ES_NETWORK_PUBLISH_HOST_DETECT_REGEXP` : detect publish host ip from an regexp (ex: inet 10\..*\/24)
+* `ES_NETWORK_PUBLISH_HOST` : "null"
+* `ES_ZEN_MINIMUM_MASTER` : "1"
+* `ES_ZEN_PING_UNICAST_HOSTS` : "null"
 * `CORS_ENABLED` : "true"
 * `CORS_ORIGIN` : "*"
--- overlay/usr/local/bin/docker-entrypoint.sh
+++ overlay/usr/local/bin/docker-entrypoint.sh
@@ -3,14 +3,30 @@
 set -e
 
 declare -A ENV_CONFIG=()
+ENV_CONFIG[ES_NETWORK_PUBLISH_HOST]=${ES_NETWORK_PUBLISH_HOST:-"null"}
+ENV_CONFIG[ES_ZEN_MINIMUM_MASTER]=${ES_ZEN_MINIMUM_MASTER:-"1"}
+ENV_CONFIG[ES_ZEN_PING_UNICAST_HOSTS]=${ES_ZEN_PING_UNICAST_HOSTS:-"null"}
 ENV_CONFIG[ES_CORS_ENABLED]=${ES_CORS_ENABLED:-"true"}
 ENV_CONFIG[ES_CORS_ORIGIN]=${ES_CORS_ORIGIN:-"*"}
+
+if [ -v ES_NETWORK_PUBLISH_HOST_DETECT_REGEXP ]; then
+    IP=$(ip addr | awk -F"[ /]*" "/${ES_NETWORK_PUBLISH_HOST_DETECT_REGEXP}/{print \$3}")
+    if [ -z $IP ]; then
+        (>&2 echo "Unable to detect publish ip with regexp "$ES_NETWORK_PUBLISH_HOST_DETECT_REGEXP)
+        exit 1
+    else
+        echo "Detected publish ip "$IP
+    fi
+
+    ENV_CONFIG[ES_NETWORK_PUBLISH_HOST]=$(ip addr | awk -F"[ /]*" "/inet .*\/24/{print \$3}")
+fi
 
 for KEY in "${!ENV_CONFIG[@]}"
 do
     # echo "Configuration : \$"$KEY" set to "${ENV_CONFIG[$KEY]}
     sed -i.bak "s!\%"$KEY"\%!"${ENV_CONFIG[$KEY]}"!" /usr/share/elasticsearch/config/elasticsearch.yml
 done
+
 
 rm /usr/share/elasticsearch/config/elasticsearch.yml.bak
 
